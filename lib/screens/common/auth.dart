036b7fba2b6d0f4788c55d39c9528178b86325a8
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-06-01 21:54:07 +0530

    update

--- lib/screens/common/auth.dart
+++ lib/screens/common/auth.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:online_dars/main.dart';
